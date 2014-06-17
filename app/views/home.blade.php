7b00cbc5c05c19f221c775ae836656b3dd20f884
Ahmed Aboulez <AhmedAboulez@AhmedSQLs-MacBook-Pro.local>
2014-06-17 18:48:49 +0200

    dates fixes and removing storge from the repo

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="utf-8">
 	<title>{{{$name}}} | {{{$job}}}</title>
-	
+
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 	<!-- jQuery -->
 	<script src="{{{asset('js/jquery.min.js')}}}"></script><!-- local jquery -->
-	<!-- 
+	<!--
 	online version of jquery
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 -->
 
 <!-- Bootstrap -->
@@ -49,15 +49,15 @@
 	<link rel="stylesheet" type="text/css" href="./css/ie.css" />
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	
+
 	<!-- PRINT CSS -->
 	<link type="text/css" rel="stylesheet" href="{{{asset('css/print.css')}}}" media="print">
-	
-	
+
+
 </head>
 
 <body class="backgrounded" data-spy="scroll" data-target=".sidebar-nav">
-	
+
 	<div class="navbar navbar-inverse navbar-fixed-top visible-phone noprint" id="phone-navbar">
 		<div class="navbar-inner">
 			<div class="container">
@@ -86,9 +86,9 @@
 
 	<div class="container">
 		<div class="row" id="content">
-			
-			
-			
+
+
+
 			<div class="span3 sbleft noprint" id="sidebar">
 				<div class="sidebar-nav equal hidden-phone">
 					<ul class="nav nav-list affix" id="resumenav">
@@ -105,17 +105,17 @@
 				</div><!--/sidebar-nav -->
 				<span class="clear"></span>
 			</div><!--/span3 -->
-			
+
 
 			<!-- MAIN CONTENT -->
 			<div class="span9 equal" id="main-content">
-				
+
 				<div class="tools noprint visible-desktop">
 					<a href="javascript:if(window.print)window.print()" title="Print" data-placement="bottom" class="tips glyphicons print"><i></i></a>
 					<a href="#" title="Download" data-placement="bottom" class="tips glyphicons file_import"><i></i></a>
 				</div>
 
-				
+
 				<div class="section first-section" id="about-me">
 					<div class="row author">
 						<div class="span3">
@@ -126,13 +126,13 @@
 							<h2 class="primary-color">{{{$job}}}</h2>
 						</div>
 					</div>
-					
-					
-					
+
+
+
 					<div class="well">
 						<p class="lead">{{nl2br($summary)}}</p>
 					</div>
-					
+
 					<div class="social-media noprint">
 						<h5>Follow me</h5>
 						<div>
@@ -141,25 +141,25 @@
 							<div class="tips icon-btn" title="Linkedin" data-placement="bottom"><a href="{{{$linkedin}}}" target="_blank" class="glyphicons white linked_in"><i></i></a></div>
 							<div class="tips icon-btn" title="github" data-placement="bottom"><a href="{{{$github}}}" target="_blank" class="glyphicons white github"><i></i></a></div>
 						</div>
-					</div>		
-				</div><!--/section--> 
-				
+					</div>
+				</div><!--/section-->
+
 				<div class="section" id="personal-information">
 					<h2 class="section-title"><span class="glyphicons user"><i></i></span>Personal Information<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
-					
+
 					<div class="row">
 						<div class="infoline span9 first"><div class="row">
 							<div class="span2 muted">Date of birth</div>
 							<div class="span7">{{{$dob}}}</div>
 						</div></div>
 						<span class="clear"></span>
-						
+
 						<div class="infoline span9"><div class="row">
 							<div class="span2 muted">Place of Birth</div>
 							<div class="span7">{{{$pob}}}</div>
 						</div></div>
 						<span class="clear"></span>
-						
+
 						<div class="infoline span9"><div class="row">
 							<div class="span2 muted">Nationallty</div>
 							<div class="span7">
@@ -167,43 +167,43 @@
 							</div>
 						</div></div>
 						<span class="clear"></span>
-						
+
 						<div class="infoline span9 last"><div class="row">
 							<div class="span2 muted">Lives in</div>
 							<div class="span7">{{{$livesin}}}</div>
 						</div></div>
 						<span class="clear"></span>
-						
+
 					</div>
-				</div><!--/section--> 
-				
+				</div><!--/section-->
+
 				<div class="section" id="employment">
 					<h2 class="section-title"><span class="glyphicons share_alt"><i></i></span>Employment<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
-					
+
 					<div class="timeline">
-						
+
 						@foreach ($employment as $position)
 						<div class="timeline-item">
-							<div class="timeline-item-date">{{{$position->start}}} - {{{$position->end}}}</div>
+						<div class="timeline-item-date">{{{date('Y', strtotime($position->start))}}} - {{{date('Y', strtotime($position->end))}}}</div>
 							<div class="timeline-item-trigger"><span class="glyphicons white circle_minus" data-toggle="collapse" data-target="#position{{{$position->id}}}"><i></i></span></div>
 							<div class="timeline-arrow"><i></i></div>
 							<div class="timeline-item-content">
 								<h3 class="timeline-item-title" data-toggle="collapse" data-target="#position{{{$position->id}}}">{{{$position->role}}} @ {{{$position->company_name}}}</h3>
 								<div class="collapse in" id="position{{{$position->id}}}">
-									<p><small class="muted">from 2011 to present day</small></p>
-									{{$position->descr}}	
+									<p><small class="muted">{{{$position->start}}} - {{{$position->end}}}</small></p>
+									{{$position->descr}}
 								</div>
 							</div>
-						</div><!--/timeline-item--> 
+						</div><!--/timeline-item-->
 						<span class="clear"></span>
 						@endforeach
-					</div><!--/timeline --> 
+					</div><!--/timeline -->
 				</div><!--/section-->
-				
-				
+
+
 				<div class="section" id="education">
 					<h2 class="section-title"><span class="glyphicons certificate"><i></i></span>Education<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
-					
+
 					<div class="timeline">
 						@foreach ($education as $course)
 						<div class="timeline-item">
@@ -216,15 +216,15 @@
 									<h4 class="media-heading primary-color">{{{$course->certificate_name}}}</h4>
 								</div>
 							</div>
-						</div><!--/timeline-item--> 
+						</div><!--/timeline-item-->
 						<span class="clear"></span>
 						@endforeach
-						
-					</div><!--/timeline --> 
-					
+
+					</div><!--/timeline -->
+
 				</div><!--/section-->
-				
-				
+
+
 				<div class="section skills" id="skills">
 					<h2 class="section-title"><span class="glyphicons cogwheels"><i></i></span>Skills<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
 					@foreach ($skills as $skill)
@@ -238,9 +238,9 @@
 						</div>
 					</div>
 					@endforeach
-					
+
 				</div><!--/section-->
-				
+
 				<div class="section" id="testimonials">
 					<h2 class="section-title"><span class="glyphicons comments"><i></i></span>Testimonials<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
 					@foreach ($testimonials as $testimonial)
@@ -256,13 +256,13 @@
 						</div>
 					</div>
 					@endforeach
-					
-				</div><!--/section-->
-				
-				
-				
 
-				
+				</div><!--/section-->
+
+
+
+
+
 				<div class="section" id="projects">
 					<h2 class="section-title"><span class="glyphicons comments"><i></i></span>Projects<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
 					@foreach ($projects as $project)
@@ -281,38 +281,38 @@
 						</div>
 					</div>
 					@endforeach
-					
+
 				</div><!--/section-->
-				
+
 				<span class="clear"></span>
 				<div class="section hobbies" id="hobbies">
 					<h2 class="section-title"><span class="glyphicons heart_empty"><i></i></span>Hobbies<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
-					
+
 					<div class="row">
 						<div class="span9">
 							@foreach ($hobbies as $hobby)
 							<span class="label"><span class="glyphicons white {{{$hobby->icon}}}"><i></i>{{{$hobby->name}}}</span></span>
 							@endforeach
-							
+
 						</div>
 					</div>
-					
-				</div><!--/section-->	  
+
+				</div><!--/section-->
 				{{--
 				<span class="clear"></span>
 				<div class="section noprint" id="contact">
 					<h2 class="section-title"><span class="glyphicons message_flag"><i></i></span>Contact<a href="#phone-navbar" class="top visible-phone"><span class="glyphicons white up_arrow"><i></i></span></a></h2>
-					
-					
+
+
 					<div class="alert alert-info">
 						<button type="button" class="close" data-dismiss="alert">&times;</button>
 						Make sure to fill all the fields with correct values before sending your email!
 					</div>
-					
+
 					<form method="post" action="#contact" class="form-horizontal">
 						<fieldset>
 							<legend>Send me a message using the form below</legend>
-							
+
 							<div class="control-group">
 								<label class="control-label" for="form-subject">Subject</label>
 								<div class="controls">
@@ -332,7 +332,7 @@
 								</div>
 							</div>
 
-							
+
 							<div class="control-group">
 								<label class="control-label" for="form-message">Message</label>
 								<div class="controls">
@@ -342,23 +342,23 @@
 									</div>
 								</div>
 							</div>
-							
+
 							<div class="control-group">
 								<div class="controls">
 									<button type="submit" class="btn btn-primary input-medium" name="submit" id="contact-submit">Submit</button>
 								</div>
 							</div>
-							
-							
+
+
 						</fieldset>
 					</form>
 				</div><!--/section-->--}}
-				
+
 			</div><!--/span9-->
 			<!-- END OF THE MAIN CONTENT -->
-			
+
 		</div><!--/row-->
-		
+
 		<div id="footer">
 			<p>&copy; Ahmed Abouelezz - {{{date('Y')}}}</p>
 		</div>
